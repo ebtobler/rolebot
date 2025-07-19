9d9e6368ad5950dbcb21ab59121aafd32954fde7
ejt1037 <ejt1037@unh.edu>
2025-07-19 12:13:30 -0400

    barebones bot that can run, but does nothing

---
+++ rolebot/main.py
@@ -0,0 +1,12 @@
+import os
+import discord
+from dotenv import load_dotenv
+
+if __name__ == "__main__":
+    load_dotenv()
+    DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+    intents = discord.Intents.default()
+    intents.members = True
+    intents.message_content = True
+    bot = discord.Client(intents=intents)
+    bot.run(token=DISCORD_TOKEN)
